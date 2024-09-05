b9659330c6271bbdc1cc4f5894875b80f3735c69
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-09-05 20:53:57 +1000

    terraform integration added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "223306781sit722week802"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version {
+  default = "1.30.3"    
+}
+ 
+
